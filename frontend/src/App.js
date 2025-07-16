86fac0c1d5ee9b848f2f74f8a4d81e6dba6689b9
dibyamgr <dibyamagar5@gmail.com>
2025-07-16 14:39:46 -0230

    Add logo and dashboard image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,8 +33,8 @@ import routes from "routes";
 import { useAppUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
 // Images
-import brandWhite from "assets/images/logo-ct.png";
-import brandDark from "assets/images/logo-ct-dark.png";
+import brandWhite from "assets/images/logos/logo-white.png";
+import brandDark from "assets/images/logos/logo-white.png";
 
 export default function App() {
   const [controller, dispatch] = useAppUIController();
@@ -138,7 +138,7 @@ export default function App() {
             <Sidenav
               color={sidenavColor}
               brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-              brandName="Material Dashboard 2"
+              brandName="ParkSmart"
               routes={routes}
               onMouseEnter={handleOnMouseEnter}
               onMouseLeave={handleOnMouseLeave}
@@ -162,7 +162,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Material Dashboard 2"
+            brandName="ParkSmart"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
