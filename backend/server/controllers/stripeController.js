8f4919b2b88b101a46466b9e0c5bee317d06e937
dibyamgr <dibyamagar5@gmail.com>
2025-07-19 16:47:25 -0230

    feat: Stripe Integration For Test

---
+++ backend/server/controllers/stripeController.js
@@ -0,0 +1,41 @@
+const getStripe = require("../utils/stripePayment");
+
+const FE_DOMAIN = "http://localhost:3000";
+
+const createCheckoutSession = async ({ invoiceId, amount, description }) => {
+  const stripe = getStripe();
+
+  try {
+    const stripeSession = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "cad",
+            product_data: {
+              name: "Parking Session Payment",
+              description: description,
+            },
+            unit_amount: Math.round(amount * 100), // cents
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      metadata: {
+        invoiceId: invoiceId,
+      },
+      success_url: `${FE_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${FE_DOMAIN}/my-bookings`,
+    });
+
+    return stripeSession.url;
+  } catch (error) {
+    console.error("Error creating Stripe checkout session:", error);
+    throw new Error("Failed to create Stripe payment session.");
+  }
+};
+
+module.exports = {
+  createCheckoutSession,
+};
