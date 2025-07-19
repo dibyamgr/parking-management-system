d6182e1055fcdfe68b133ba2e4f59626d2b942f6
Irin-Sultana <irins@mun.ca>
2025-07-19 15:43:17 -0230

    log test

--- backend/server/app.js
+++ backend/server/app.js
@@ -8,6 +8,7 @@ const parkingSlotRoutes = require("./routes/parkingSlotRoutes");
 const vehicleRoutes = require("./routes/vehicleRoutes");
 const parkingSessionRoutes = require("./routes/parkingSessionRoutes");
 const invoiceRoutes = require("./routes/invoiceRoutes");
+const logRoutes = require('./routes/logRoutes');
 const paymentStatusRoutes = require("./routes/paymentStatusRoutes");
 
 dotenv.config();
@@ -23,6 +24,7 @@ app.use("/api/parking-slots", parkingSlotRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 app.use("/api/parking-sessions", parkingSessionRoutes);
 app.use("/api/invoices", invoiceRoutes);
+app.use('/api/logs', logRoutes);
 app.use("/api/admin/payment-statuses", paymentStatusRoutes);
 
 module.exports = app;
